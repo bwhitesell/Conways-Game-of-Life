ea1cff204bf2c0e30dcdd17cd3af2d7cef93e824
Ben Whitesell <whitesell.ben@gmail.com>
2022-05-22 20:01:14 -0700

    updates

--- lireddit-web/src/components/Board.tsx
+++ lireddit-web/src/components/Board.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Button } from '@chakra-ui/react';
 import React from 'react';
 import GridCell  from '../components/GridCell'
 
@@ -9,45 +9,119 @@ interface BoardProps {
 }
 
 interface BoardState {
-  cells: undefined;
+  grid: JSX.Element[] | void;
 }
 
 class Board extends React.Component<BoardProps, BoardState> {
   nVerticalCells: number;
   nHorizontalCells: number;
+  cellSkeleton: boolean[][];
 
   constructor(props: BoardProps) {
     super(props)
 
     this.nVerticalCells = props.nVerticalCells;
     this.nHorizontalCells = props.nHorizontalCells;
+    this.cellSkeleton = this.generateGrid()
     this.state = {
-      cells: undefined
+      grid: this.renderGrid(true),
     }
-
   }
 
   nBoardCells() {
     return this.nVerticalCells * this.nHorizontalCells
   }
 
-  generateGrid() {
+ generateGrid() {
+    return Array(
+      this.nVerticalCells
+    ).fill([]).map(
+      x => Array(this.nHorizontalCells).fill(false)
+    )
+  }
+
+  renderGrid(returnGrid: boolean = false): JSX.Element[] | void {
+    // init new grid obj
     const grid = [];
-    for (let i = 0; i < this.nVerticalCells; i++) {
-      const row = [];
-      for (let j = 0; j < this.nHorizontalCells; j++) {
-        row.push(<GridCell x={i} y={j} />)
+
+    // construct grid obj from skeleton
+    for (let rowNum = 0; rowNum < this.nVerticalCells; rowNum++) {
+      const renderedGridRow = [];
+      for (let colNum = 0; colNum < this.nHorizontalCells; colNum ++) {
+        const cellIsAlive = this.cellSkeleton[rowNum][colNum]
+        const cellOnClickCB = this.buildSetGridCellCB(rowNum, colNum);
+        const cellKey = String(rowNum + colNum) + String(cellIsAlive)
+        renderedGridRow.push(
+          <GridCell key={cellKey} alive={cellIsAlive} onClick={cellOnClickCB} />
+        )
       }
-      grid.push(<Box margin="auto" flex={1}>{row}</Box>)
+      grid.push(
+        <Box key={rowNum} display="flex">{renderedGridRow}</Box>
+      )
     }
-    return grid
+
+    // return grid or update grid state?
+    if (returnGrid) {
+      return grid
+    } else {
+      this.setState({grid: grid})
+    }
+  }
+ 
+  getNeighbors(x: number, y: number) {
+    const neighbors = []
+    for (let i = -1; i <= 1; i ++){
+      for (let j = -1; j <= 1; j ++) {
+        if (i !== 0 || j !== 0) {
+          if (this.coordinatesInBounds(x + i, y + j)) {
+              neighbors.push(this.cellSkeleton[x + i][y + j])
+            }
+        }
+      }
+    }
+    return neighbors
+  }
+
+  coordinatesInBounds(x: number, y: number) {
+    if (x >= 0 && x < this.nVerticalCells && y >= 0 && y < this.nHorizontalCells) {
+      return true
+    }
+    return false
+  }
+
+  buildSetGridCellCB(x: number, y: number) {
+    return () => {
+      this.cellSkeleton[x][y] = !this.cellSkeleton[x][y]
+      this.renderGrid()
+    }
+  }
+
+  updateGridSkeleton() {
+    // update skeleton
+    for (let rowNum = 0; rowNum < this.nVerticalCells; rowNum ++) {
+      for (let colNum = 0; colNum < this.nHorizontalCells; colNum++) {
+        const nNeighbors = this.getNeighbors(rowNum, colNum).filter(Boolean).length
+        const cellIsAlive = this.cellSkeleton[rowNum][colNum];
+        if (cellIsAlive) {
+          if (nNeighbors > 3) {
+            this.cellSkeleton[rowNum][colNum] = false;
+          } else if ( nNeighbors < 2) {
+            this.cellSkeleton[rowNum][colNum] = false;
+          }
+        } else if (!cellIsAlive && nNeighbors === 3) {
+          this.cellSkeleton[rowNum][colNum] = true;
+        }
+      }
+    }
+    // rerender the grid
+    this.renderGrid();
   }
 
   render() {
-
     return (
       <Box display="block" justifyContent="center" alignContent="vertical">
-        {this.generateGrid()}
+        {this.state.grid}
+        <Button onClick={() => this.updateGridSkeleton()}>update grid</Button>
       </Box>
     )
   }
--- lireddit-web/src/components/GridCell.tsx
+++ lireddit-web/src/components/GridCell.tsx
@@ -3,31 +3,92 @@ import React from 'react';
 
 
 interface GridCellProps {
-  x: number;
-  y: number;
+  alive: boolean;
+  animateToState: boolean;
+  onClick: () => void;
 }
 
 interface GridCellState {
-  x: number;
-  y: number;
-  alive: boolean;
+  backgroundPositionX: string;
 }
 
 
 class GridCell extends React.Component<GridCellProps, GridCellState> {
+  onClick: () => void;
+  alive: boolean;
+  cellSize: number
+  animation: () => void
 
   constructor(props: GridCellProps) {
     super(props);
+    this.alive = props.alive;
+    this.onClick = props.onClick;
+
+    this.animation = this.alive ? this.animateBirth : this.animateDeath
+    this.cellSize = 64;
+
     this.state = {
-      x: props.x,
-      y: props.y,
-      alive: false,
-    };
+      backgroundPositionX: this.cellSizeInPixels(this.alive ? 0 : -33),
+    }
   }
+
+  cellSizeInPixels(multiple: number = 1) {
+    return String(this.cellSize * multiple) + 'px'
+  }
+
+  alivePosition() {
+    return this.cellSizeInPixels(33)
+  }
+
+  deadPosition() {
+    return this.cellSizeInPixels(0)
+  }
+
+  componentDidMount() {
+    this.animation()
+  }
+
+  async animateBirth() {
+    for (let i = 0; i < 34; i++) {
+      await new Promise(resolve => setTimeout(
+        () => {
+          this.setState({backgroundPositionX: this.cellSizeInPixels(-i)});
+          resolve('');
+        },
+        50,
+      ));
+    }
+  }
+
+  async animateDeath() {
+    for (let i = -33; i < 1; i++) {
+      await new Promise(resolve => setTimeout(
+        () => {
+          this.setState({backgroundPositionX: this.cellSizeInPixels(i)});
+          resolve('');
+        },
+        50,
+      ));
+    }
+  }
+
 
   render() {
     return (
-      <Button backgroundColor="blue" flex={1} borderRadius={0}/>
+      <Box w={this.cellSizeInPixels()} h={this.cellSizeInPixels()} overflow="hidden">
+        <Button
+          width={this.cellSizeInPixels(34)}
+          height={this.cellSizeInPixels()}
+          backgroundPosition={this.state.backgroundPositionX}
+          backgroundSize="100%"
+          backgroundImage='/growth.png'
+          backgroundColor="white"
+          flex={1}
+          p={0}
+          borderRadius={0}
+          onClick={(e: React.MouseEvent) => this.onClick()}
+        />
+      </Box>
     )
   }
 }
